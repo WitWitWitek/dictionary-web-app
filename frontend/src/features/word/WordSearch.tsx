66aa27f0c2b98e6c0cee480d06136f3fdd17a3bb
Witold <witekrychlik@o2.pl>
2023-06-16 23:03:50 +0200

    color schemes basics added

--- frontend/src/features/word/WordSearch.tsx
+++ frontend/src/features/word/WordSearch.tsx
@@ -5,7 +5,7 @@ import WordOutput from './WordOutput/WordOutput';
 export default function WordSearch() {
   const [wordData, setWordData] = useState<WordData | null>(null);
   return (
-    <section>
+    <section className="theme-default">
       <WordForm setWordData={setWordData} />
       <WordOutput wordData={wordData} />
     </section>
