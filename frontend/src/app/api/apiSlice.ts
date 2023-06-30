0306cc00db14c6195d7ab581ce4fccb69ff68164
Witold <witekrychlik@o2.pl>
2023-06-30 07:58:43 +0200

    fetch query function modified to send token data

--- frontend/src/app/api/apiSlice.ts
+++ frontend/src/app/api/apiSlice.ts
@@ -1,9 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import { RootState } from '../store';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:3500',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const { token } = (getState() as RootState).auth;
+    if (token) {
+      headers.set('authentication', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions,
+) => baseQuery(args, api, extraOptions);
 
 const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
-  tagTypes: [],
+  baseQuery,
+  tagTypes: ['User'],
   endpoints: () => ({}),
 });
 
