a19f5c7a79ac403a71d06d5a0364694797b3e9f6
Witold <witekrychlik@o2.pl>
2023-07-02 10:26:32 +0200

    getAllRepetitions route added

--- backend/src/controllers/repetitionController.ts
+++ backend/src/controllers/repetitionController.ts
@@ -3,6 +3,19 @@ import { AppDataSource } from "../dataSource";
 import { Repetition } from "../entity/Repetition";
 import { CustomError } from "../utils/customError";
 
+export const getAllRepetitions: RequestHandler = async (req, res) => {
+  try {
+    const reqepetitionsRepository = await AppDataSource.manager.getRepository(
+      Repetition
+    );
+
+    const repetitions = await reqepetitionsRepository.find();
+    return res.status(201).json(repetitions);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const addNewRepetition: RequestHandler = async (req, res) => {
   const { content } = req.body;
   if (!content || typeof content !== "string") {
--- backend/src/router/repetitionsRouter.ts
+++ backend/src/router/repetitionsRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { addNewRepetition } from "../controllers/repetitionController";
+import {
+  addNewRepetition,
+  getAllRepetitions,
+} from "../controllers/repetitionController";
 import authMiddleware from "../middleware/authMiddleware";
 
 const repetitionsRouter = Router();
 
 repetitionsRouter.use(authMiddleware);
-repetitionsRouter.route("/").post(addNewRepetition);
+repetitionsRouter.route("/").get(getAllRepetitions).post(addNewRepetition);
 
 export default repetitionsRouter;
