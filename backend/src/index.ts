42957e9d8f96dbb58a23f56dcd29d81b8255e115
Witek <witekrychlik@o2.pl>
2023-08-27 09:54:42 +0200

    hotfix in import aliases

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 /* istanbul ignore file */
+import "module-alias/register";
 import { config } from "dotenv";
 config();
 
-import { AppDataSource } from "@/dataSource";
-import app from "@/app";
+import { AppDataSource } from "./dataSource";
+import app from "./app";
 
 AppDataSource.initialize()
   .then(() => app.listen(3500, () => console.log("server listening on port 3500")))
