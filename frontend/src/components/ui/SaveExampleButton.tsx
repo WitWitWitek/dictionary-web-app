8227d541dc8174221d71651dd4915e76396c91ee
Witold <witekrychlik@o2.pl>
2023-10-23 20:54:57 +0200

    saveButton styled

--- frontend/src/components/ui/SaveExampleButton.tsx
+++ frontend/src/components/ui/SaveExampleButton.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { FaPlus, FaCheck } from 'react-icons/fa6';
 import { selectCurrentUser } from '@/features/auth/authSlice';
 import { usePostRepetitionMutation } from '@/features/repetition/repetitionApiSlice';
 
@@ -8,7 +9,7 @@ type Props = {
 
 export default function SaveExampleButton({ exampleContent }: Props) {
   const user = useSelector(selectCurrentUser);
-  const [postRepetition] = usePostRepetitionMutation();
+  const [postRepetition, { isSuccess }] = usePostRepetitionMutation();
 
   const postRepetitionHandler = async () => {
     await postRepetition({ content: exampleContent });
@@ -17,8 +18,14 @@ export default function SaveExampleButton({ exampleContent }: Props) {
   if (!user) return null;
 
   return (
-    <button type="button" onClick={postRepetitionHandler}>
-      save
+    <button
+      type="button"
+      className="meanings__save-btn"
+      onClick={postRepetitionHandler}
+      title="Add repetition to collection."
+      disabled={isSuccess}
+    >
+      {!isSuccess ? <FaPlus /> : <FaCheck />}
     </button>
   );
 }
