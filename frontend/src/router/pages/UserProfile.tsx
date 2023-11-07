5b579015b19a1fc7f4e73ee0c2806cf08f3cbd81
Witold <witekrychlik@o2.pl>
2023-11-07 20:59:12 +0100

    styling to userProfile page added

--- frontend/src/router/pages/UserProfile.tsx
+++ frontend/src/router/pages/UserProfile.tsx
@@ -14,21 +14,34 @@ export default function UserProfile() {
 
   return (
     <div className="user-profile">
-      <h1>Your profile:</h1>
-      <div>
-        <h2>Username: {userData?.username}</h2>
-        <p>Repetitions amount: {userData?.repetitonsCount}</p>
-        <p>Excercises done: {userData?.excercisesCount}</p>
+      <h1 className="user-profile__heading">Your profile:</h1>
+      <div className="user-profile__container user-profile__container--user">
+        <h2>
+          Username: <span className="user-profile__container-data">{userData?.username}</span>
+        </h2>
+        <p>
+          Repetitions amount: <span className="user-profile__container-data">{userData?.repetitonsCount}</span>
+        </p>
+        <p>
+          Excercises done: <span className="user-profile__container-data">{userData?.excercisesCount}</span>
+        </p>
       </div>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/user-repetitions">Your Repetitions</Link>
-      <div>
+      <div className="user-profile__container user-profile__container--menu">
+        <h3>Repetitions menu:</h3>
+        <Link to="/dashboard" className="user-profile__container-btn">
+          Dashboard
+        </Link>
+        <Link to="/user-repetitions" className="user-profile__container-btn">
+          Your Repetitions
+        </Link>
+      </div>
+      <div className="user-profile__container user-profile__container--password">
         <h3>Change Password:</h3>
         <UpdatePasswordForm />
       </div>
-      <div>
+      <div className="user-profile__container user-profile__container--delete">
         <h3>Delete your account:</h3>
-        <button type="button" onClick={deleteUserHandler}>
+        <button type="button" onClick={deleteUserHandler} className="user-profile__container-btn--delete">
           Delete
         </button>
       </div>
