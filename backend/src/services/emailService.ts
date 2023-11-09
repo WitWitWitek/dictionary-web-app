aee48b462a0f864827625a2cc2df643d198f595d
Witold <witekrychlik@o2.pl>
2023-11-09 22:06:25 +0100

    ejs view engine implemented to sending emails

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -3,16 +3,33 @@ import { CustomError } from "@/utils/customError";
 import { mailOptions, transporter } from "@/utils/nodemailer";
 import { signToken } from "@/utils/tokenHandlers";
 import { findUser } from "./userService";
+import * as ejs from "ejs";
+import * as path from "path";
+
 export async function sendVerificationEmail(email: string, username: string) {
   const emailToken = signToken(username, "email");
-
-  try {
-    await transporter.sendMail({
-      ...mailOptions(email, `http://localhost:5173/confirm?token=${emailToken}`),
-    });
-  } catch (err) {
-    const incorrectUser = await findUser(username);
-    await incorrectUser.remove();
-    throw new CustomError("Something went wrong", HTTP_CODES.INTERNAL_SERVER_ERROR);
-  }
+  const link = `http://localhost:5173/confirm?token=${emailToken}`;
+  const filePath = path.resolve(process.cwd() + "/src/templates/userConfirmationEmail.ejs");
+  ejs.renderFile(
+    filePath,
+    {
+      username,
+      link,
+    },
+    async (renderError, view) => {
+      if (renderError) {
+        throw new CustomError("Something went wrong", HTTP_CODES.INTERNAL_SERVER_ERROR);
+      } else {
+        try {
+          await transporter.sendMail({
+            ...mailOptions(email, view),
+          });
+        } catch (err) {
+          const incorrectUser = await findUser(username);
+          await incorrectUser.remove();
+          throw new CustomError("Something went wrong", HTTP_CODES.INTERNAL_SERVER_ERROR);
+        }
+      }
+    },
+  );
 }
