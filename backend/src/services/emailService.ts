15d4d3051eb19869a977c31144ca881b211563e7
Witold <witekrychlik@o2.pl>
2023-11-09 19:52:39 +0100

    deleting incorectly created user implementen in sendEmail

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -2,7 +2,7 @@ import { HTTP_CODES } from "@/types";
 import { CustomError } from "@/utils/customError";
 import { mailOptions, transporter } from "@/utils/nodemailer";
 import { signToken } from "@/utils/tokenHandlers";
-
+import { findUser } from "./userService";
 export async function sendVerificationEmail(email: string, username: string) {
   const emailToken = signToken(username, "email");
 
@@ -11,6 +11,8 @@ export async function sendVerificationEmail(email: string, username: string) {
       ...mailOptions(email, `http://localhost:5173/confirm?token=${emailToken}`),
     });
   } catch (err) {
+    const incorrectUser = await findUser(username);
+    await incorrectUser.remove();
     throw new CustomError("Something went wrong", HTTP_CODES.INTERNAL_SERVER_ERROR);
   }
 }
